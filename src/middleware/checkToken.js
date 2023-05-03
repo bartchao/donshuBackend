4b342edd03629397821f649b6c409d7ff78d546d
bartchao <59232607+bartchao@users.noreply.github.com>
2023-05-03 11:33:15 +0800

    checkToken back to normal

--- src/middleware/checkToken.js
+++ src/middleware/checkToken.js
@@ -7,21 +7,12 @@ module.exports = (req, res, next) => {
     const bearer = bearerHeader.split(" ");
     const bearerToken = bearer[1];
     req.token = bearerToken;
-    const testPayload = {
-      id: "25f24979-4167-4e0b-b602-0e6a527ce513",
-      role: 0
-    };
-    req.user = testPayload;
-    return next();
-    /* verifyToken(req.token)
+    verifyToken(req.token)
       .then(decoded => {
         const { payload } = decoded;
-        const testPayload = {
-          id: "30ddf50c-4c53-492d-ae27-6d8fcdce19a0"
-        };
-        req.user = testPayload;
+        req.user = payload;
         return next();
       })
-      .catch(err => res.status(401).send(err)); */
+      .catch(err => res.status(401).send(err));
   } else res.status(401).send({ sucess: false, message: "VerifyTokenExpiredFailed" });
 };
