790ff33e6647ae1f8e96b18aca3a96cc60b5475c
bartchao <59232607+bartchao@users.noreply.github.com>
2023-05-24 18:07:52 +0800

    use express.static for serving static files

--- src/app.js
+++ src/app.js
@@ -47,17 +47,7 @@ if (process.env.NODE_ENV !== "production") {
     format: winston.format.simple()
   }));
 }
-/* const fs = require("fs");
-const rfs = require("rotating-file-stream");
-const logDirectory = path.join(__dirname, "../log");
-// ensure log directory exists
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-// create a rotating write stream
-const accessLogStream = rfs.createStream("access.log", {
-  interval: "1d", // rotate daily
-  path: logDirectory,
-  maxFiles: 30
-}); */
+
 
 const app = express();
 
@@ -72,9 +62,7 @@ app.use(helmet());
 app.use(cors());
 
 app.use("/", indexRouter);
-
 app.use(checkAPIkey);
-
 app.use("/public", publicRouter);
 app.use("/private", privateRouter);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,44 +1,10 @@
-const fs = require("fs");
 const path = require("path");
-const { errHandler } = require("../helper/errHandler.js");
 const express = require("express");
 const router = express.Router();
-const sanitize = require("sanitize-filename");
 
 router.get("/", (req, res, next) => res.send("Hello!!"));
 
-router.get("/uploads/:upload", function (req, res) {
-  const file = sanitize(req.params.upload);
-  //console.log(req.params.upload);
-  try {
-    console.log(__dirname);
-    const img = fs.readFileSync(path.join(__dirname, "../../uploads/") + file);
-    // console.log("!!!!");
-    // console.log(img);
-    res.writeHead(200, { "Content-Type": "image/*" });
-    res.end(img, "binary");
-  } catch (err) {
-    console.log(err);
-    errHandler(err, res);
-  }
-});
-
-router.get("/apk/:apkFile", function (req, res) {
-  const file = sanitize(req.params.apkFile);
-  const dirpath = path.join(__dirname, "../../apk/");
-  //console.log(req.params.apkFile);
-  try {
-    const apk = fs.readFileSync(path.join(dirpath, file));
-    // console.log("!!!!");
-    // console.log(apk);
-    res.writeHead(200, { "Content-Type": "application/vnd.android.package-archive" });
-    res.end(apk);
-  } catch (err) {
-    console.log(err);
-    errHandler(err, res);
-  }
-  // res download error 有點難處理，而且網路差時只會傳一半...
-  // res.download(path.join(dirpath, file),file,err=>{if(err)console.error(err);});
-});
+router.use("/uploads",express.static(path.join(__dirname, "../../uploads/")));
+router.use("/apk",express.static(path.join(__dirname, "../../apk/")));
 
 module.exports = router;
